b049e72668ca95e22c38a93d7d68be1d4e1e4b44
samho <samhocngz@gmial.com>
2021-03-31 22:53:33 +0800

    1. Add testing app. 2. Add testing models. 3. update application app models with testing app mapping.

--- application/models.py
+++ application/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Application(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     comment = models.CharField(max_length=300, null=True)
+    version = models.CharField(max_length=20)
     created_at = models.DateTimeField
 
     def __str__(self):
@@ -20,3 +21,10 @@ class ApplicationPort(models.Model):
         return "Application: %d -> Port: %d" % (self.app_id, self.port)
 
 
+class ApplicationFeatures(models.Model):
+    app_id = models.IntegerField(db_index=True)
+    feature_id = models.IntegerField(db_index=True)
+    comment = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return "Application: %d -> feature: %d" % (self.app_id, self.feature_id)
